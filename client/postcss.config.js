43549a2ea49cd8affd3c86d9d4d906c5a58b8f13
masa08 <ushimasarug08@gmail.com>
2021-12-16 22:07:32 +0900

    set css minify

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,11 +1,13 @@
 const postcssImport = require('postcss-import');
 const postcssPresetEnv = require('postcss-preset-env');
 const tailwindcss = require('tailwindcss');
+const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
     postcssImport(),
     tailwindcss(),
+    cssnano(),
     postcssPresetEnv({
       stage: 3,
     }),
